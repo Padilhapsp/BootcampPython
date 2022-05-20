f9c8fd6a05d84e1e280f66e164fc86b56c349539
padilhapsp <padilhapsp@hotmail.com>
2022-05-19 21:39:59 -0300

    Adiciona funcionalidade de criar tarefas

--- tests/test_gerenciador.py
+++ tests/test_gerenciador.py
@@ -81,15 +81,18 @@ def test_quando_listar_tarefas_a_tarefa_retornada_deve_possuir_um_estado():
     assert "estado" in resposta.json().pop()
     TAREFAS.clear()
 
+
 def test_recurso_tarefas_deve_aceitar_o_verbo_post():
     cliente = TestClient(app)
     resposta = cliente.post("/tarefas")
     assert resposta.status_code != status.HTTP_405_METHOD_NOT_ALLOWED
 
+
 def test_quando_uma_tarefa_e_submetida_deve_possuir_um_titulo():
     cliente = TestClient(app)
     resposta = cliente.post("/tarefas", json={})
     assert resposta.status_code == status.HTTP_422_UNPROCESSABLE_ENTITY
+
 
 def test_titulo_da_tarefa_deve_conter_entre_3_e_50_caracteres():
     cliente = TestClient(app)
@@ -98,15 +101,20 @@ def test_titulo_da_tarefa_deve_conter_entre_3_e_50_caracteres():
     resposta = cliente.post("/tarefas", json={"titulo": 51 * "*"})
     assert resposta.status_code == status.HTTP_422_UNPROCESSABLE_ENTITY
 
+
 def test_quando_uma_tarefa_e_submetida_deve_possuir_uma_descricao():
     cliente = TestClient(app)
     resposta = cliente.post("/tarefas", json={"titulo": "titulo"})
     assert resposta.status_code == status.HTTP_422_UNPROCESSABLE_ENTITY
 
+
 def test_descricao_da_tarefa_pode_conter_no_maximo_140_caracteres():
     cliente = TestClient(app)
-    resposta = cliente.post("/tarefas", json={"titulo": "titulo", "descricao": "*" * 141})
+    resposta = cliente.post(
+        "/tarefas", json={"titulo": "titulo", "descricao": "*" * 141}
+    )
     assert resposta.status_code == status.HTTP_422_UNPROCESSABLE_ENTITY
+
 
 def test_quando_criar_uma_tarefa_a_mesma_deve_ser_retornada():
     cliente = TestClient(app)
@@ -117,6 +125,7 @@ def test_quando_criar_uma_tarefa_a_mesma_deve_ser_retornada():
     assert tarefa_criada["descricao"] == tarefa_esperada["descricao"]
     TAREFAS.clear()
 
+
 def test_quando_criar_uma_tarefa_seu_id_deve_ser_unico():
     cliente = TestClient(app)
     tarefa1 = {"titulo": "titulo1", "descricao": "descricao1"}
@@ -126,12 +135,14 @@ def test_quando_criar_uma_tarefa_seu_id_deve_ser_unico():
     assert resposta1.json()["id"] != resposta2.json()["id"]
     TAREFAS.clear()
 
+
 def test_quando_criar_uma_tarefa_seu_estado_padrao_e_nao_finalizado():
     cliente = TestClient(app)
     tarefa = {"titulo": "titulo", "descricao": "descricao"}
     resposta = cliente.post("/tarefas", json=tarefa)
     assert resposta.json()["estado"] == "não finalizado"
     TAREFAS.clear()
+
 
 def test_quando_criar_uma_tarefa_codigo_de_status_retornado_deve_ser_201():
     cliente = TestClient(app)
@@ -140,16 +151,10 @@ def test_quando_criar_uma_tarefa_codigo_de_status_retornado_deve_ser_201():
     assert resposta.status_code == status.HTTP_201_CREATED
     TAREFAS.clear()
 
+
 def test_quando_criar_uma_tarefa_esta_deve_ser_persistida():
     cliente = TestClient(app)
     tarefa = {"titulo": "titulo", "descricao": "descricao"}
     cliente.post("/tarefas", json=tarefa)
     assert len(TAREFAS) == 1
     TAREFAS.clear()
-
-
-
-
-
-
-
